f26100db9df33b195e372964be0c22fdf1ff6fac
gyanna081 <pacanageanna@gmail.com>
2024-12-08 22:29:25 +0800

    making changes

--- brgy/venv/Scripts/activate.bat
+++ brgy/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Murdered Cupcake\Desktop\AI\Barangay-Information-Management-System-BIMS-\brgy\venv
+set VIRTUAL_ENV=C:\Users\Geanna\IdeaProjects\Barangay-Information-Management-System-BIMS-\brgy\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
